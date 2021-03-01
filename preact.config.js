180791b9d59aa81f67ef4009f0461c8d8f65a6a4
Drarig29 <corentingirard.dev@gmail.com>
2021-03-01 13:50:44 +0000

    Deploying to gh-pages from  @ c28d89a0d36fe79d3176cd49e15051d345641119 🚀

--- preact.config.js
+++
@@ -1,7 +0,0 @@
-import { DefinePlugin } from 'webpack';
-
-export default function (config, env, helpers) {
-  const PUBLIC_PATH = env.production ? '/kounter/' : '';
-  config.output.publicPath = PUBLIC_PATH;
-  config.plugins.push(new DefinePlugin({ PUBLIC_PATH }));
-}
